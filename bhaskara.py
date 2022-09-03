98992ff92f814a914c8439f3361311e3cde5bfbc
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2022-09-02 21:38:13 -0300

    vertice

--- bhaskara.py
+++ bhaskara.py
@@ -16,23 +16,27 @@ class CalcRaiz:
         except(TypeError, ValueError):
             print("Insira valores validos")
         else:
-            print(a, b, c)
             self.delta = (self.b*self.b)-4*self.a*self.c
             if self.delta > 0:
                 self.raiz=2
             elif self.delta == 0:
                 self.raiz=1
-            elif self.delta < 0:
+            if self.delta < 0:
                 self.raiz=-1
-            self.calcBhaskara()
+            else:
+                print(self.calcBhaskara())
+            print(self.calcVertice())
 
     def calcBhaskara(self):
         if self.raiz == 2 or self.raiz == 1:
             self.raizUm = (-self.b+sqrt(self.delta))/2*self.a
             if self.raiz == 2:
                 self.raizDois = (-self.b-sqrt(self.delta))/2*self.a
-        elif self.raiz == -1:
-            return 'null'
-        print(self.raizUm, self.raizDois)
+        return self.raizUm, self.raizDois
+
+    def calcVertice(self):
+        self.xv = -self.b/(2*self.a)
+        self.yv = -self.delta/(4*self.a)
+        return self.xv, self.yv
 
 delta = CalcRaiz(a, b, c)
